9900a742fbe72e231e3faf7b1470ec7d826f060e
Tresor1H <monseigneur140@gmail.com>
2025-12-12 17:24:07 +0000

    ajout des autres fichiers

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\SDK flutter\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\ekjle\OneDrive\Bureau\5semestre\aerosafe"
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\tchaa\AeroSafe"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\SDK flutter\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\ekjle\OneDrive\Bureau\5semestre\aerosafe"
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\tchaa\AeroSafe"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
