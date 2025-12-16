0df8a06de5fe96740e3911054fcdf7773bd5be46
theo385 <abrahamwaal523@gmail.com>
2025-12-16 00:16:17 +0000

    Update generated_config.cmake

--- windows/flutter/ephemeral/generated_config.cmake
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -1,6 +1,6 @@
 # Generated code do not commit.
-file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
-file(TO_CMAKE_PATH "C:\\Users\\tchaa\\AeroSafe" PROJECT_DIR)
+file(TO_CMAKE_PATH "C:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Abraham WA-ALOUN\\Documents\\GitHub\\AeroSafe" PROJECT_DIR)
 
 set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
 set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
@@ -10,15 +10,15 @@ set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
 
 # Environment variables to pass to tool_backend.sh
 list(APPEND FLUTTER_TOOL_ENVIRONMENT
-  "FLUTTER_ROOT=C:\\src\\flutter"
-  "PROJECT_DIR=C:\\Users\\tchaa\\AeroSafe"
-  "FLUTTER_ROOT=C:\\src\\flutter"
-  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\tchaa\\AeroSafe\\windows\\flutter\\ephemeral"
-  "PROJECT_DIR=C:\\Users\\tchaa\\AeroSafe"
-  "FLUTTER_TARGET=C:\\Users\\tchaa\\AeroSafe\\lib\\main.dart"
-  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzUuNQ==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049YWM0ZTc5OWQyMw==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049ZDNkNDVkY2YyNQ==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My45LjI="
+  "FLUTTER_ROOT=C:\\flutter"
+  "PROJECT_DIR=C:\\Users\\Abraham WA-ALOUN\\Documents\\GitHub\\AeroSafe"
+  "FLUTTER_ROOT=C:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Abraham WA-ALOUN\\Documents\\GitHub\\AeroSafe\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Abraham WA-ALOUN\\Documents\\GitHub\\AeroSafe"
+  "FLUTTER_TARGET=C:\\Users\\Abraham WA-ALOUN\\Documents\\GitHub\\AeroSafe\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9WRVJTSU9OPTMuMzguNQ==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049ZjZmZjE1MjlmZA==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049MTUyN2FlMGVjNQ==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMC40"
   "DART_OBFUSCATION=false"
   "TRACK_WIDGET_CREATION=true"
   "TREE_SHAKE_ICONS=false"
-  "PACKAGE_CONFIG=C:\\Users\\tchaa\\AeroSafe\\.dart_tool\\package_config.json"
+  "PACKAGE_CONFIG=C:\\Users\\Abraham WA-ALOUN\\Documents\\GitHub\\AeroSafe\\.dart_tool\\package_config.json"
 )
